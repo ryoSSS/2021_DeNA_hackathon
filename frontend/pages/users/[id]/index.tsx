585f93b6f16dde86c6c4e555c5fa7cb848e22fe4
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-19 01:42:41 +0900

    ✨ update form page

--- frontend/pages/users/[id]/index.tsx
+++ frontend/pages/users/[id]/index.tsx
@@ -1,89 +1,145 @@
-import { NextPage } from "next";
+import { NextPage, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import getUser from "../../../utils/getUser";
 import createMessage from "../../../utils/createMessage";
 import useUser from "../../../hooks/useUser";
 import { User } from "../../../models";
 import GlassmorphismBox from "../../../components/GlassmorphismBox";
+import Error from "next/error";
 
-const DUMMY_USER: User = {
-	id: 1,
-	name: "taro",
-	birthday: "2000/01/01",
-	messages: [
-		{
-			id: 1,
-			content: "abcdefg".repeat(100),
-		},
-		{
-			id: 2,
-			content: "bbbbb",
-		},
-		{
-			id: 3,
-			content: "ccccc",
-		},
-	],
+type UsersIdProps = {
+	status: number;
+	data?: User;
 };
 
-const UsersId: NextPage = () => {
+const UsersId: NextPage<UsersIdProps> = (props) => {
 	const router = useRouter();
-	const { id: userId } = router.query;
 	const { user, setUser } = useUser();
-	const [error, setError] = useState(false);
+	const [text, setText] = useState<string>("");
 
-	useEffect(() => {
-		if (userId === undefined) return;
-		if (user !== undefined) return;
-
-		(async () => {
-			const res = await getUser(Number(userId));
-			if (res.error) {
-				setError(true);
-			}
+	const getAgain = async (user_id: number) => {
+		// SSRした後の再フェッチ
+		const res = await getUser(user_id);
+		if (res.error) {
+			alert("エラーが発生が発生しました");
+			return;
+		} else {
 			setUser(res.data);
-		})();
-	}, [userId, setUser, user]);
-
-	const onClick = () => {
-		const content = "お誕生日おめでとう！！！";
-		createMessage(Number(userId), content);
+		}
 	};
 
-	if (error) {
-		<p>error</p>;
+	const onClick = () => {
+		createMessage(props.data.id, text);
+		alert("送信しました！");
+		setText("");
+		getAgain(props.data.id);
+	};
+
+	if (props.status !== 200) {
+		return <Error statusCode={props.status} />;
 	}
 
 	return (
-		<div
-			className="object-contain w-full h-screen"
-			style={{
-				backgroundImage: `url('/star.png')`,
-				backgroundSize: "cover",
-				backgroundPosition: "center center",
-				backgroundRepeat: "no-repeat",
-			}}
-		>
-			<button onClick={onClick}>送信</button>
+		// <div
+		// 	className="object-contain w-full h-screen"
+		// 	style={{
+		// 		backgroundImage: `url('/elnaz-asadi-RczTV4UAkw0-unsplash.jpg')`,
+		// 		backgroundSize: "cover",
+		// 		backgroundPosition: "center center",
+		// 		backgroundRepeat: "no-repeat",
+		// 	}}
+		// >
+		<div className="bg-gradient-to-b from-indigo-300 to-blue-500">
+			<div className="pt-10">
+				<GlassmorphismBox className="mx-4">
+					<div className="">
+						<textarea
+							className="w-full px-4 py-4 text-lg bg-transparent focus:outline-none"
+							name="form"
+							placeholder="入力ホーム"
+							id="form"
+							cols={20}
+							rows={5}
+							value={text}
+							onChange={(e) => setText(e.target.value)}
+						></textarea>
+					</div>
+				</GlassmorphismBox>
+			</div>
 
-			<div className="pt-[160px]"></div>
-			<ul className="w-11/12 mx-auto flex flex-col space-y-6">
-				{user &&
-					user.messages.map((message) => (
-						<li key={message.id}>
-							<GlassmorphismBox className="h-[180px] px-6 py-6">
-								<div className="h-full overflow-y-scroll break-words">
-									<p className="text-white text-center max-w-full my-auto">
-										{message.content}
-									</p>
-								</div>
-							</GlassmorphismBox>
-						</li>
-					))}
-			</ul>
+			<GlassmorphismBox className="mx-4 my-4">
+				<div className="text-center">
+					<button
+						onClick={onClick}
+						className="px-4 py-2 font-semibold text-gray-700 focus:outline-none active:outline-none"
+					>
+						送信
+					</button>
+				</div>
+			</GlassmorphismBox>
+
+			<GlassmorphismBox className="mx-4 my-4">
+				<div className="text-center">
+					<button
+						onClick={() => router.push(`/users/${props.data.id}/messages`)}
+						className="px-4 py-2 font-semibold text-gray-700 focus:outline-none active:outline-none"
+					>
+						メッセージ一覧へ
+					</button>
+				</div>
+			</GlassmorphismBox>
+
+			<div className="pb-10">
+				<ul className="flex flex-col mx-4 mx-auto space-y-6">
+					{user ? (
+						<>
+							{user.messages.map((message) => (
+								<li key={message.id}>
+									<GlassmorphismBox className="h-[180px] px-6 py-6">
+										<div className="h-full overflow-y-scroll break-words">
+											<p className="max-w-full my-auto text-center text-white">
+												{message.content}
+											</p>
+										</div>
+									</GlassmorphismBox>
+								</li>
+							))}
+						</>
+					) : (
+						<>
+							{props.data.messages.map((message) => (
+								<li key={message.id}>
+									<GlassmorphismBox className="h-[180px] px-6 py-6">
+										<div className="h-full overflow-y-scroll break-words">
+											<p className="max-w-full my-auto text-center text-white">
+												{message.content}
+											</p>
+										</div>
+									</GlassmorphismBox>
+								</li>
+							))}
+						</>
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
 
 export default UsersId;
+
+export const getServerSideProps: GetServerSideProps<any> = async (
+	context: any
+) => {
+	try {
+		const { id } = context.query;
+		console.log(id);
+		const res = await getUser(id);
+		if (res.error) return { props: { status: 404, data: {} } };
+		console.log(res.data);
+		return { props: { status: 200, data: res.data } };
+	} catch (e) {
+		return { props: { status: 500, data: {} } };
+	}
+};
