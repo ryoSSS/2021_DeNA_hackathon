c6e88b6965b3c97f7af4b3fc701c35a8b87f7a2b
yamori-masato <yamorimasato@gmail.com>
2021-09-19 08:26:01 +0900

    update /users/:user_id page

---
+++ frontend/components/CloseButton.tsx
@@ -0,0 +1,38 @@
+import { ComponentPropsWithRef, forwardRef } from "react";
+import clsx from "clsx";
+
+type Props = ComponentPropsWithRef<"button">;
+
+// eslint-disable-next-line react/display-name
+const CloseButton = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+	const { className, ...rest } = props;
+	return (
+		<button
+			type="button"
+			className={clsx(
+				"bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500",
+				className
+			)}
+			{...rest}
+		>
+			<span className="sr-only">Close menu</span>
+			<svg
+				className="h-6 w-6"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				aria-hidden="true"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		</button>
+	);
+});
+
+export default CloseButton;
