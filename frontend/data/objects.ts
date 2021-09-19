21590fc7b4f8bfdc7b6162fccff5c21aa7fe6d05
yamori-masato <yamorimasato@gmail.com>
2021-09-19 11:36:45 +0900

    register temporary data

---
+++ frontend/data/objects.ts
@@ -0,0 +1,16 @@
+type ObjectType = {
+	id: number;
+	name: string;
+	imageSrc: string;
+};
+
+const OBJECTS: ObjectType[] = [
+	{ id: 1, name: "wine", imageSrc: "/gift-amico.svg" },
+	{ id: 2, name: "cake", imageSrc: "/gift-box.svg" },
+	{ id: 3, name: "candle", imageSrc: "/gift-box.svg" },
+	{ id: 4, name: "balloon", imageSrc: "/gift-amico.svg" },
+	{ id: 5, name: "sticker", imageSrc: "/gift-amico.svg" },
+	{ id: 6, name: "partyPopper", imageSrc: "/gift-box.svg" },
+];
+
+export default OBJECTS;
