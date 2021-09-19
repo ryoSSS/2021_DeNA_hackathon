56c348ad3077fcedb9a2078c97015664dadee929
FujimuraKaito <kaito.bakuman@gmail.com>
2021-09-19 09:16:08 +0900

    💄 update tailwind sort

--- frontend/components/MessageBox.tsx
+++ frontend/components/MessageBox.tsx
@@ -8,12 +8,12 @@ type Props = {
 const MessageBox: VFC<Props> = ({ children, writerName }) => {
 	return (
 		<div className="relative bg-white h-[160px] px-3 py-6 border-4 border-blue-100 rounded-sm overflow-hidden">
-			<div className="h-full flex items-center">
-				<p className="max-h-full overflow-auto max-w-full mx-auto text-sm text-center text-black break-words">
+			<div className="flex items-center h-full">
+				<p className="max-w-full max-h-full mx-auto overflow-auto text-sm text-center text-black break-words">
 					{children}
 				</p>
 			</div>
-			<p className="absolute bottom-1 right-2 text-xs text-gray-800">
+			<p className="absolute text-xs text-gray-800 bottom-1 right-2">
 				<span className="mr-2 text-gray-500">from:</span>
 				{writerName}
 			</p>
